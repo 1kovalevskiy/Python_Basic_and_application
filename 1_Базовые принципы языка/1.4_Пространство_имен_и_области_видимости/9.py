b2542c81a0f6634f456b37294ec8301490d681b9
1kovalevskiy <koval701@gmail.com>
2019-01-17 22:41:30 +0700

    done

--- 1_Базовые принципы языка/1.4_Пространство_имен_и_области_видимости/9.py
+++ 1_Базовые принципы языка/1.4_Пространство_имен_и_области_видимости/9.py
@@ -1,12 +1,29 @@
 def create(namespace, parent):
-
+    dictionary[parent][2].append(namespace)
+    dictionary[namespace] = [[], parent, []]
+    return None
 
 def add(namespace, var):
-
+    dictionary[namespace][0].append(var)
+    return None
 
 def get(namespace, var):
+    flag = 0
+    if namespace != 'global':
+        flag = dictionary[namespace][0].count(var)
+        if flag > 0:
+            print(namespace)
+        else:
+            get(dictionary[namespace][1], var)
+    else:
+        flag = dictionary[namespace][0].count(var)
+        if flag > 0:
+            print(namespace)
+        else:
+            print(None)
+    return None
 
-
+dictionary = {'global':[[], None, []]}
 n = int(input())
 
 for i in range(n):
@@ -21,4 +38,4 @@ for i in range(n):
     elif cmd == "get":
         get(namespace, argument)
 
-    
+    print(dictionary)
