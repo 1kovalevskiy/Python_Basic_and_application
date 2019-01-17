f6b6af9f9f9afc354cfb35629fa8a2770d06864b
1kovalevskiy <36358311+1kovalevskiy@users.noreply.github.com>
2019-01-17 18:48:28 +0700

    fix 1.4.9

--- 1_Базовые принципы языка/1.4_Пространство_имен_и_области_видимости/9.py
+++ 1_Базовые принципы языка/1.4_Пространство_имен_и_области_видимости/9.py
@@ -1,1 +1,26 @@
+def create(namespace, parent):
+
+
+def add(namespace, var):
+
+
+def get(namespace, var):
+
+
 n = int(input())
+
+for i in range(n):
+    cmd, namespace, argument = input().split()
+
+    if cmd == "create":
+        create(namespace, argument)
+
+    elif cmd == "add":
+        add(namespace, argument)
+
+    elif cmd == "get":
+        get(namespace, argument)
+
+    
+
+print(command)
