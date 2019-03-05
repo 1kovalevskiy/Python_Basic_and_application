f1a6c5821b3f6aa5b6b9a98270ed4aae2c4f7ee1
Kovalevskiy <koval701@gmail.com>
2019-03-05 11:49:04 +0700

    fix

--- 3/3.4/6.py
+++ 3/3.4/6.py
@@ -1,2 +1,16 @@
-import requests
+import requests, re
 
+fr = input()
+to = input()
+linkRE = r"<a href=['\"]([\w/:\.]+)['\"]"
+
+res = requests.get(fr)
+links = re.findall(linkRE, res.text)
+for link in links:
+	res = requests.get(link)
+	links = re.findall(linkRE, res.text)
+	if to in links:
+		print("Yes")
+		break
+else:
+	print("No")
